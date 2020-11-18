cd4989188f0fae223dbd5b3ccd336706e0a8ac5a
xsmolova <smolova@positive.sk@users.noreply.github.com>
2020-11-18 14:49:47 +0100

    Updates

---
+++ precache-manifest.885d7a63392740c1f9144c572ac51048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f7ca7b9114eb76988e349ea6a85d54",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5152718c3e10d54a3bc9",
+    "url": "/Monsters-rolodex/static/css/main.502fd950.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5152718c3e10d54a3bc9",
+    "url": "/Monsters-rolodex/static/js/main.64f8c378.chunk.js"
+  },
+  {
+    "revision": "ce236435b1a77080e948",
+    "url": "/Monsters-rolodex/static/js/runtime-main.9e6da6d0.js"
+  }
+]);
